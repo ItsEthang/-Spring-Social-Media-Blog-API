b08ab56c94b46e873bdc27a8e272397426ccc375
Ethan Tang <itsethang5@gmail.com>
2024-09-01 05:50:14 +0000

    First init

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,9 @@
 package com.example.service;
 
 public class AccountService {
+    //1: Our API should be able to process new User registrations.
+
+    //2: Our API should be able to process User logins.
+
+    
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,15 @@
 package com.example.service;
 
 public class MessageService {
+    //Our API should be able to process the creation of new messages.
+
+    //Our API should be able to retrieve all messages.
+
+    //Our API should be able to retrieve a message by its ID.
+
+    //Our API should be able to delete a message identified by a message ID.
+
+    //Our API should be able to update a message text identified by a message ID.
+
+    //Our API should be able to retrieve all messages written by a particular user.
 }
