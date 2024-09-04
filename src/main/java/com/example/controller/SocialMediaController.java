8e0a541d048dfed1286284738d1df26ed33a98db
Ethan Tang <itsethang5@gmail.com>
2024-09-04 21:47:56 +0000

    I did it!

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -38,6 +39,7 @@ public class SocialMediaController {
     private MessageService messageService;
     private AccountService accountService;
 
+    @Autowired
     public SocialMediaController(MessageService messageService, AccountService accountService) {
         this.messageService = messageService;
         this.accountService = accountService;
