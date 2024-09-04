bca21d77a242b4b1a514d9356108ebea295478a0
Ethan Tang <itsethang5@gmail.com>
2024-09-04 01:42:01 -0400

    Create services and repositories for messages and accounts

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,9 +1,34 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
-    //1: Our API should be able to process new User registrations.
+    AccountRepository accountRepository;
 
-    //2: Our API should be able to process User logins.
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
-    
+    // 1: Our API should be able to process new User registrations.
+    public Account accountRegister(Account account) {
+        return accountRepository.save(account);
+    }
+
+    // 2: Our API should be able to process User logins.
+    // Todo: make a custom query
+    public Account accountLogin(String username, String password) {
+        Optional<Account> validAccount = accountRepository.findAccountByUsernameAndPassword(username, password);
+        if (validAccount.isPresent()) {
+            return validAccount.get();
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,15 +1,63 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
-    //Our API should be able to process the creation of new messages.
+    MessageRepository messageRepository;
 
-    //Our API should be able to retrieve all messages.
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
-    //Our API should be able to retrieve a message by its ID.
+    // Our API should be able to process the creation of new messages.
+    public Message messageCreate(Message message) {
+        return messageRepository.save(message);
+    }
 
-    //Our API should be able to delete a message identified by a message ID.
+    // Our API should be able to retrieve all messages.
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
 
-    //Our API should be able to update a message text identified by a message ID.
+    // Our API should be able to retrieve a message by its ID.
+    public Message getMessageById(Integer id) {
+        Optional<Message> optionalMessage = messageRepository.findById(id);
+        if (optionalMessage.isPresent()) {
+            return optionalMessage.get();
+        }
+        return null;
 
-    //Our API should be able to retrieve all messages written by a particular user.
+    }
+
+    // Our API should be able to delete a message identified by a message ID.
+    public void deleteMessage(Integer id) {
+        messageRepository.deleteById(id);
+    }
+
+    // Our API should be able to update a message text identified by a message ID.
+    public void updateMessage(Integer id, Message replacement) {
+        Optional<Message> optionalMessage = messageRepository.findById(id);
+        if (optionalMessage.isPresent()) {
+            Message message = optionalMessage.get();
+            message.setMessageText(replacement.getMessageText());
+            messageRepository.save(message);
+        }
+
+    }
+
+    // Our API should be able to retrieve all messages written by a particular user.
+    // Todo: make custom query
+    public List<Message> getMessagesByUser(Integer postedBy) {
+        return messageRepository.findMessagesByPostedBy(postedBy);
+    }
+
 }
